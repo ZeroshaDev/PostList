94a9b263f323cd9da0c7f64c1ce161823b37dd2d
Zerosha <soldatumora@gmail.com>
2022-07-30 00:24:51 +0300

    pagination rework and server change, loading placeholder

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const fetchData = async () => {
-  let dat = await fetch("https://jsonplaceholder.typicode.com/posts");
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
+
+const fetchData = async (skiped, limit) => {
+  let dat = await fetch(
+    `https://dummyjson.com/posts?skip=${skiped}&limit=${limit}`
+  );
   dat = await dat.json();
-  return [dat, dat.length];
+  return { data: dat.posts, total: dat.total };
 };
 
 function postLoader(dat) {
@@ -27,47 +37,191 @@ function postLoader(dat) {
     postBody.classList.add("postBody");
     postBody.textContent = item.body;
 
+    let postTags = document.createElement("p");
+    postTags.classList.add("postTags");
+    postTags.textContent = "#" + item.tags.join(" #");
+
+    let postReactions = document.createElement("div");
+    postReactions.classList.add("postReactions");
+    let img = document.createElement("img");
+    img.src = "rsource/favorite_FILL0_wght300_GRAD-25_opsz20.svg";
+    postReactions.append(img);
+    postReactions.append(item.reactions);
+
     li.append(userId);
     li.append(postId);
     li.append(postTitle);
     li.append(postBody);
+    li.append(postTags);
+    li.append(postReactions);
     ul.append(li);
   });
   document.body.append(ul);
 }
 
-function pagination(count, data) {
+function pagination(count) {
   let paginator = document.querySelector(".paginator");
   let cntr = count % 10 ? count / 10 + 1 : count / 10;
   for (let i = 0; i < cntr; ++i) {
-    let paginatorItem = document.createElement("div");
-    paginatorItem.classList.add("paginatorItem");
-    paginatorItem.textContent = `${i + 1}`;
-    paginator.append(paginatorItem);
+    if (i === 4) {
+      let paginatorItem = document.createElement("div");
+      paginatorItem.classList.add("paginatorSkiper");
+      paginatorItem.textContent = `...`;
+      paginator.append(paginatorItem);
+    } else {
+      if (i === cntr - 1) {
+        let paginatorItem = document.createElement("div");
+        paginatorItem.classList.add("paginatorItem");
+        paginatorItem.textContent = `${i + 1}`;
+        paginator.append(paginatorItem);
+      } else {
+        if (i < 4) {
+          let paginatorItem = document.createElement("div");
+          paginatorItem.classList.add("paginatorItem");
+          paginatorItem.textContent = `${i + 1}`;
+          paginator.append(paginatorItem);
+        } else {
+          continue;
+        }
+      }
+    }
   }
   paginator.addEventListener("click", (e) => {
     if (e.target.classList == "paginatorItem") {
       document.querySelector(".postList").remove();
-      document.querySelectorAll(".paginatorItem").forEach((item)=>{
-        item.classList.remove("paginatorItemCurrent");
+      document.querySelectorAll(".paginatorItem").forEach((item) => {
+        item.classList.remove("active");
       });
-      postLoader(
-        data.slice(
-          e.target.textContent == 1 ? 0 : e.target.textContent * 10 - 10,
-          e.target.textContent * 10
-        )
-      );
-      e.target.classList.add("paginatorItemCurrent");
+      render((e.target.textContent - 1) * 10, 10);
+      paginatorReRender(e.target.textContent, cntr);
     }
-    console.log(e.target);
   });
 }
 
-const mainFunction = async () => {
-  const dat = await fetchData();
-  pagination(dat[1], dat[0]);
-  postLoader(dat[0].slice(0, 10));
-  document.querySelector(".paginatorItem").classList.add("paginatorItemCurrent");
+function paginatorReRender(current, last) {
+  let paginator = document.querySelector(".paginator");
+  if (current <= 3) {
+    while (paginator.firstChild) {
+      paginator.removeChild(paginator.firstChild);
+    }
+    for (let i = 0; i < last; ++i) {
+      if (i === 4) {
+        let paginatorItem = document.createElement("div");
+        paginatorItem.classList.add("paginatorSkiper");
+        paginatorItem.textContent = `...`;
+        paginator.append(paginatorItem);
+      } else {
+        if (i === last - 1) {
+          let paginatorItem = document.createElement("div");
+          paginatorItem.classList.add("paginatorItem");
+          paginatorItem.textContent = `${i + 1}`;
+          paginator.append(paginatorItem);
+        } else {
+          if (i < 4) {
+            let paginatorItem = document.createElement("div");
+            paginatorItem.classList.add("paginatorItem");
+            paginatorItem.textContent = `${i + 1}`;
+            if (i + 1 == current) {
+              paginatorItem.classList.add("active");
+            }
+            paginator.append(paginatorItem);
+          } else {
+            continue;
+          }
+        }
+      }
+    }
+  } else {
+    if (current >= last - 2) {
+      while (paginator.firstChild) {
+        paginator.removeChild(paginator.firstChild);
+      }
+      for (let i = 0; i < last; ++i) {
+        if (i === 1) {
+          let paginatorItem = document.createElement("div");
+          paginatorItem.classList.add("paginatorSkiper");
+          paginatorItem.textContent = `...`;
+          paginator.append(paginatorItem);
+        } else {
+          if (i === 0) {
+            let paginatorItem = document.createElement("div");
+            paginatorItem.classList.add("paginatorItem");
+            paginatorItem.textContent = `${i + 1}`;
+            paginator.append(paginatorItem);
+          } else {
+            if (i >= last - 4) {
+              let paginatorItem = document.createElement("div");
+              paginatorItem.classList.add("paginatorItem");
+              paginatorItem.textContent = `${i + 1}`;
+              if (i + 1 == current) {
+                paginatorItem.classList.add("active");
+              }
+              paginator.append(paginatorItem);
+            } else {
+              continue;
+            }
+          }
+        }
+      }
+    } else {
+      if (paginator.childElementCount < 9) {
+        while (paginator.childElementCount !== 9) {
+          let paginatorItem = document.createElement("div");
+          paginatorItem.classList.add("paginatorItem");
+          paginatorItem.textContent = `.`;
+          paginator.append(paginatorItem);
+        }
+      }
+      let currentCounter = -2;
+      paginator.childNodes.forEach((item, i) => {
+        item.classList = "";
+        switch (i) {
+          case 0:
+            item.textContent = 1;
+            item.classList.add("paginatorItem");
+            break;
+          case 1:
+            item.textContent = "...";
+            item.classList.add("paginatorSkiper");
+            break;
+          case 7:
+            item.textContent = "...";
+            item.classList.add("paginatorSkiper");
+            break;
+          case 8:
+            item.textContent = last;
+            item.classList.add("paginatorItem");
+            break;
+          default:
+            item.textContent = Number(current) + currentCounter;
+            if (Number(current) + currentCounter == current) {
+              item.classList.add("active");
+            }
+            item.classList.add("paginatorItem");
+            currentCounter++;
+            break;
+        }
+      });
+    }
+  }
+}
+
+const app = async () => {
+  const dat = await fetchData(0, 10);
+  pagination(dat.total);
+  postLoader(dat.data);
+  document.querySelector(".paginatorItem").classList.add("active");
 };
 
-mainFunction();
+async function render(skip, limit) {
+  document.body.classList.remove("loaded");
+  const dat = await fetchData(skip, limit);
+  postLoader(dat.data);
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+}
+
+app();
